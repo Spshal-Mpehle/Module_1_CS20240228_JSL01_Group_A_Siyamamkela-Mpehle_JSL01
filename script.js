6e638667baca0398295cc2b5f059b108f38a0753
Siyamamkela <mpehlesiyamamkela@gmail.com>
2024-10-25 07:07:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let result = 'Valid Syntax';
+    let petInput = 'pet_numAlpha';
+
+    if (petInput == pet_numAlpha){
+        console.log (result);
+    } else {
+        console.log ('Invalid Syntax');
+    } 
+
+     // Placeholder for validation result
+
 
     // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
+        // Check if input starts with 'pet_' and followed  by alphanumeric characters
 }
-
-
